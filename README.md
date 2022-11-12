660206dcdcec5b2731eedb87f7d5a775b9cde3df
Uriel Infante <ur13l.infante@gmail.com>
2022-11-12 14:56:57 -0600

    Doc update
    Docs update for a more clear re-implementation.

--- README.md
+++ README.md
@@ -1,26 +1,30 @@
 # promo-bot-mx
 
-Bot para Telegram y Twitter que revisa ofertas de videojuegos y tecnología publicadas en 
-https://promodescuentos.com y las genera un feed que se actualiza con las entradas más recientes. 
+Bot para Telegram y Twitter que revisa ofertas de videojuegos y tecnología publicadas en
+https://promodescuentos.com y genera un feed que se actualiza con las entradas más recientes.
 
 ## Funciones
 
 ### GetHotPromos
+
 Realiza una consulta a las primeras páginas de las categorías seleccionadas en la página de Promodescuentos para comparar si
 estas ya existen en base de datos, en caso de que sean nuevas se realiza el envío de mensaje para que sea publicado con un bot de Telegram.
 Esta función está programada para ejecutarse constantemente durante el día.
 
 ### DeleteHotPromos
+
 Se encarga de realizar la limpieza de las ofertas viejas, cada día se realiza la ejecución de esta función para revisar aquellas
 ofertas que lleven varios días en la base de datos y eliminarlas.
 
 ## Configuración del proyecto
-Para realizar modificaciones y probar el proyecto de manera local es necesario tener instalado el CLI de SAM 
+
+Para realizar modificaciones y probar el proyecto de manera local es necesario tener instalado el CLI de SAM
 (https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html) y Docker
-en nuestro equipo. 
+en nuestro equipo.
 
 ### Base de datos local
-En caso de que se desee hacer pruebas con una base de datos local, el proyecto cuenta con una imagen de DynamoDB local para su ejecución en Docker con configuración en el archivo 
+
+En caso de que se desee hacer pruebas con una base de datos local, el proyecto cuenta con una imagen de DynamoDB local para su ejecución en Docker con configuración en el archivo
 _docker-compose.yml_. Si no se desea trabajar con una base de datos local, el proyecto ya cuenta con la instrucción para generar la base de datos como parte del stack de CloudFormation, por lo tanto, estos pasos no serían necesarios.
 
 ```
@@ -40,30 +44,33 @@ networks:
   local-dev:
     driver: bridge
 
-``` 
+```
 
 De forma que para ejecutar la base de datos a nivel local solo se debe ejecutar el siguiente comando
 desde la carpeta raíz del proyecto.
 
 ```
 $ docker-compose up -d
-``` 
+```
 
-Una vez levantada, deberíamos tener acceso a la base de datos en el puerto 8000 del equipo, si por alguna razón no es posible 
-acceder a la información, se procede a crear la tabla con el siguiente comando.
+Una vez desplegada, contaremos con acceso a la base de datos en el puerto 8000 del equipo. La primera vez que se ejecuta el proyecto o si no es posible
+acceder a la información, se tiene que crear una tabla específica para el guardado de ofertas con el siguiente comando el siguiente comando.
+
+**NOTA:** Es necesario contar con la [herramienta de ejecución de comandos](https://aws.amazon.com/es/cli/) de AWS para este proceso.
 
 ```
 aws dynamodb --endpoint-url http://localhost:8000 create-table --table-name promo_bot_mx_promos --key-schema AttributeName=id,KeyType=HASH AttributeName=created_at,KeyType=RANGE --attribute-definitions AttributeName=id,AttributeType=S AttributeName=created_at,AttributeType=N  --billing-mode PAY_PER_REQUEST
 ```
 
-La base de datos se encontrará disponible en el puerto 8000 de nuestro equipo. Se recomienda utilizar una herramienta 
-como [DynamoDBGUI](https://github.com/Arattian/DynamoDb-GUI-Client) o [NoSQL Workbench](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.html) 
-para un mejor control de la información.
+La base de datos se encontrará disponible en el puerto 8000 de nuestro equipo. Se recomienda utilizar una herramienta
+como [DynamoDBGUI](https://github.com/Arattian/DynamoDb-GUI-Client) o [NoSQL Workbench](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.html)
+para la consulta y administración de los datos.
 
 ### Archivos de entorno
+
 Es posible generar diferentes entornos para la ejecución del proyecto en formato JSON.
 En el caso de este proyecto, es necesario generar un archivo _env-local.json_ en la raíz del proyecto (se puede tomar como ejemplo
-el archivo env.example.json), y debe contar con una estructura como la siguiente:
+el archivo _env.example.json_), y debe contar con una estructura como la siguiente:
 
 ```
     {
@@ -86,47 +93,57 @@ el archivo env.example.json), y debe contar con una estructura como la siguiente
     }
 ```
 
-Lo ideal es generar un entorno por default con las configuraciones para la ejecución local. 
+Lo ideal es generar un entorno por default con las configuraciones para la ejecución local.
 
 ### Invocar funciones
+
 La invocación de las funciones de manera local se puede realizar de la siguiente forma.
+
 1. Se necesita generar un build del proyecto, esto se puede realizar con el comando
 
-    ```
-    $ sam build
-    ```
+   ```
+   $ sam build
+   ```
 
 2. Una vez generada la compilación de las funciones sin error, se puede probar cada una por separado utilizando el comando
 
-    ```
+   ```
    $ sam local invoke -n env-local.json --docker-network promo-bot-mx_local-dev <Nombre de la función>
    ```
+
    Ejemplo: para ejecutar una corrida local de la función GetHotPromos, es posible con el comando
-   
+
    ```
    $ sam local invoke -n env-local.json --docker-network promo-bot-mx_local-dev GetHotPromos
    ```
 
-### Publicación 
-Para publicar cambios en las funciones de producción es necesario generar un archivo _.env_ en la raíz del proyecto 
+### Publicación
+
+Para publicar cambios en las funciones de producción es necesario generar un archivo _.env_ en la raíz del proyecto
 para proteger información sensible, en este caso, se genera un archivo con una estructura similar a la siguiente:
+
 ```
 TELEGRAM_URL=https://api.telegram.org/<API_KEY>:<API_SECRET>/sendMessage
 TELEGRAM_CHAT_ID="-123456789123"
-AWS_LOCAL_PROFILE=default 
+AWS_LOCAL_PROFILE=default
 TWITTER_APPLICATION_CONSUMER_KEY=<TWITTER_APPLICATION_CONSUMER_KEY>
 TWITTER_APPLICATION_SECRET=<TWITTER_APPLICATION_SECRET>
 TWITTER_USER_ACCESS_TOKEN=<TWITTER_USER_ACCESS_TOKEN>
 TWITTER_USER_SECRET=<TWITTER_USER_SECRET>
 ```
-Donde se reemplazarán los valores por los que coincidan con la cuenta de AWS correcta y el bot al que se apunte. Para finalizar, solo se debe ejecutar los comandos
+
+Donde se reemplazarán los valores por los que coincidan con la cuenta de AWS correcta y el bot al que se apunte. Para finalizar, solo se deben ejecutar los comandos
 
 ```
 $ source .env
+```
+
+```
 $ sam deploy --parameter-overrides "TelegramURL=$TELEGRAM_URL" "TelegramChatId=$TELEGRAM_CHAT_ID" "TwitterApplicationConsumerKey=$TWITTER_APPLICATION_CONSUMER_KEY" "TwitterApplicationSecret=$TWITTER_APPLICATION_SECRET" "TwitterUserAccessToken=$TWITTER_USER_ACCESS_TOKEN" "TwitterUserSecret=$TWITTER_USER_SECRET" --profile $AWS_LOCAL_PROFILE
 ```
 
 Existen archivos auxiliares para apoyar con la ejecución de las funciones, estos pueden ser modificados a consideración:
+
 - build.sh
 - deploy.sh
 - invoke.sh
--- get-hot-promos/app.js
+++ get-hot-promos/app.js
@@ -1,205 +1,281 @@
-'use strict'
+"use strict";
 
-const AWS = require('aws-sdk'),
-    cheerio = require('cheerio'),
-    got = require('got'),
-    Promo = require('./models/promo'),
-    sites = require('./sites'),
-    OAuth = require('oauth'),
-    PAGE_SEARCH = 3,
-    {
-        TELEGRAM_URL,
-        TELEGRAM_CHAT_ID,
-        ENDPOINT,
-        TWITTER_APPLICATION_CONSUMER_KEY,
-        TWITTER_APPLICATION_SECRET,
-        TWITTER_USER_ACCESS_TOKEN,
-        TWITTER_USER_SECRET
-    } = process.env;
+const AWS = require("aws-sdk"),
+  cheerio = require("cheerio"),
+  got = require("got"),
+  Promo = require("./models/promo"),
+  sites = require("./sites"),
+  OAuth = require("oauth"),
+  PAGE_SEARCH = 3,
+  {
+    TELEGRAM_URL,
+    TELEGRAM_CHAT_ID,
+    ENDPOINT,
+    TWITTER_APPLICATION_CONSUMER_KEY,
+    TWITTER_APPLICATION_SECRET,
+    TWITTER_USER_ACCESS_TOKEN,
+    TWITTER_USER_SECRET,
+  } = process.env;
 
-const documentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10', endpoint: ENDPOINT})
+/**
+ * Variable that allows us to consume AWS Services like DynamoDB
+ */
+const documentClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+  endpoint: ENDPOINT,
+});
 
-const compose = (...fns) => arg => fns.reduce((composed, f) => composed.then(f), Promise.resolve(arg));
+/**
+ * Compose functions to be executed one after the other.
+ * @param  {...function} fns
+ * @returns
+ */
+const compose =
+  (...fns) =>
+  (arg) =>
+    fns.reduce((composed, f) => composed.then(f), Promise.resolve(arg));
 
-const scrapURL = async (url) => {
-    const promos = [],
-        options = {timeout: 3000},
-        response = await got(url, options),
-        $ = cheerio.load(response.body, {decodeEntities: false});
+/**
+ * Iterate every page from the list looking for new offers.
+ * @param {Array} sites
+ * @returns Array
+ */
+const iterateSites = async (sites) => {
+  const results = [];
 
-    /** We filter those thread deals that are not expired */
-    $('.thread--deal:not(.thread--expired)').each((_, article) => {
-            const promo = Promo.newInstance($(article));
-            promos.push(promo);
-        }
-    )
-    return promos;
-}
-
-const iterateSites = async sites => {
-    const results = [];
-
-    /** Iteration over sites loaded from json */
-    sites.forEach(site => {
-        console.log(`Scrapping ${site.siteName} on course...`);
-        site.routes.forEach(route => {
-            console.log(`Getting data from ${route.name}...`);
-            /** We search a certain number of pages from each category */
-            for (let i = 1; i <= PAGE_SEARCH; i++) {
-                const pageParam = `?page=${i}`;
-                /** We save the promises on an array to wait for the resolution of all of them */
-                results.push(scrapURL(site.siteURL + route.path + pageParam, route.name));
-            }
-        });
+  /** Iteration over sites loaded from json */
+  sites.forEach((site) => {
+    console.log(`Scrapping ${site.siteName} on course...`);
+    site.routes.forEach((route) => {
+      console.log(`Getting data from ${route.name}...`);
+      /** We search a certain number of pages from each category */
+      for (let i = 1; i <= PAGE_SEARCH; i++) {
+        const pageParam = `?page=${i}`;
+        /** We save the promises on an array to wait for the resolution of all of them */
+        results.push(
+          scrapURL(site.siteURL + route.path + pageParam, route.name)
+        );
+      }
     });
-    return Promise.all(results);
-}
+  });
+  return Promise.all(results);
+};
 
-const mergeRetrievedPromos = async results =>
-    results.reduce((generalArray, specificArray) =>
-        [...generalArray, ...specificArray], []);
+/**
+ * Merges every site array into one containing all of the offers
+ * @param {Array} results
+ * @returns Array
+ */
+const mergeRetrievedPromos = async (results) =>
+  results.reduce(
+    (generalArray, specificArray) => [...generalArray, ...specificArray],
+    []
+  );
 
-const removeRepeated = async retrievedPromos =>
-    retrievedPromos.filter((elem, index, self) =>
-        index === self.findIndex(p =>
-            p.id === elem.id
-        )
-    )
+/**
+ * Remove repeated promos.
+ * @param {Array} retrievedPromos
+ * @returns Array
+ */
+const removeRepeated = async (retrievedPromos) =>
+  retrievedPromos.filter(
+    (elem, index, self) => index === self.findIndex((p) => p.id === elem.id)
+  );
 
-const getDBItems = async retrievedPromos => {
-    /** DynamoDB instance */
-    const params = {
-            TableName: "promo_bot_mx_promos",
-        },
-        /** Getting all promos from database as Promo objects*/
-        rawPromos = (await documentClient.scan(params).promise()).Items;
+/**
+ * Querying for promos stored on database.
+ * @param {Array} retrievedPromos
+ * @returns Object
+ */
+const getDBItems = async (retrievedPromos) => {
+  /** DynamoDB instance */
+  const params = {
+      TableName: "promo_bot_mx_promos",
+    },
+    /** Getting all promos from database as Promo objects*/
+    rawPromos = (await documentClient.scan(params).promise()).Items;
 
-    return {
-        retrievedPromos,
-        currentPromos: Promo.batchFromRaw(rawPromos)
-    }
-}
+  return {
+    retrievedPromos,
+    currentPromos: Promo.batchFromRaw(rawPromos),
+  };
+};
 
-const filterPromos = async ({retrievedPromos, currentPromos}) => {
-    /** Double filter to remove those elements retrieved that already exists on DB*/
-    return retrievedPromos.filter((promo) => {
-        const p = currentPromos.filter(promoStored => promo.id === promoStored.id);
-        return p.length === 0;
-    });
-}
-
-const storePromos = async retrievedPromos => {
-    const results = [];
-    console.log("Writing elements on database...");
-    retrievedPromos.forEach(promo => {
-            //Check if link exists before saving the new promo
-            if (promo.link) {
-                const params = {
-                    TableName: "promo_bot_mx_promos",
-                    Item: {
-                        'id': promo.id,
-                        'title': promo.title,
-                        'temp': promo.temp,
-                        'created_at': promo.created_at.getTime(),
-                        'link': promo.link,
-                        'price': promo.price
-                    }
-                }
-                results.push(documentClient.put(params).promise());
-            }
-        }
+/**
+ * Compare and filter promos from scrapping and database and remove the repeated ones.
+ * @param {Object} param0
+ * @returns Array
+ */
+const filterPromos = async ({ retrievedPromos, currentPromos }) => {
+  /** Double filter to remove those elements retrieved that already exists on DB*/
+  return retrievedPromos.filter((promo) => {
+    const p = currentPromos.filter(
+      (promoStored) =>
+        promo.id === promoStored.id || promo.title === promoStored.title
     );
-    await Promise.all(results);
-    return retrievedPromos;
-}
+    return p.length === 0;
+  });
+};
 
-const broadcast = async data => {
-    const messages = [],
-        oauth = new OAuth.OAuth(
-            'https://api.twitter.com/oauth/request_token',
-            'https://api.twitter.com/oauth/access_token',
-            TWITTER_APPLICATION_CONSUMER_KEY,
-            TWITTER_APPLICATION_SECRET,
-            '1.0A',
-            null,
-            'HMAC-SHA1'
-        )
-
-    data.forEach(promo => {
-        const {title, price, temp, link} = promo;
-        if (link) {
-            const msg = `${title} | ${price || ''} | ${temp || ''}\n ${link}`;
-            messages.push(sendMessageTelegram(msg));
-            messages.push(sendTweet(msg, oauth))
-        }
-    });
-    await Promise.all(messages);
-    return data;
-}
-
-const sendMessageTelegram = async message => {
-    const params = {
-        timeout: 3000,
-        searchParams: {
-            chat_id: TELEGRAM_CHAT_ID,
-            text: message
-        }
+/**
+ * Once filtered, it stores the new promos on the database.
+ * @param {Array} retrievedPromos
+ * @returns Array
+ */
+const storePromos = async (retrievedPromos) => {
+  const results = [];
+  console.log("Writing elements on database...");
+  retrievedPromos.forEach((promo) => {
+    //Check if link exists before saving the new promo
+    if (promo.link) {
+      const params = {
+        TableName: "promo_bot_mx_promos",
+        Item: {
+          id: promo.id,
+          title: promo.title,
+          temp: promo.temp,
+          created_at: promo.created_at.getTime(),
+          link: promo.link,
+          price: promo.price,
+        },
+      };
+      results.push(documentClient.put(params).promise());
     }
-    return got(TELEGRAM_URL, params);
-}
+  });
+  await Promise.all(results);
+  return retrievedPromos;
+};
 
+/**
+ * Generate the broadcast for Twitter and Telegram Services.
+ * @param {Array} data
+ * @returns
+ */
+const broadcast = async (data) => {
+  const messages = [],
+    oauth = new OAuth.OAuth(
+      "https://api.twitter.com/oauth/request_token",
+      "https://api.twitter.com/oauth/access_token",
+      TWITTER_APPLICATION_CONSUMER_KEY,
+      TWITTER_APPLICATION_SECRET,
+      "1.0A",
+      null,
+      "HMAC-SHA1"
+    );
+
+  data.forEach((promo) => {
+    const { title, price, temp, link } = promo;
+    if (link) {
+      const msg = `${title} | ${price || ""} | ${temp || ""}\n ${link}`;
+      messages.push(sendMessageTelegram(msg));
+      messages.push(sendTweet(`${msg}`, oauth));
+    }
+  });
+  await Promise.all(messages);
+  return data;
+};
+
+/*************************************** AUXILIAR FUNCTIONS **************************************************
+ * Functions that are not part of the compose iteration and help to obtain data required during the process. *
+ ************************************************************************************************************/
+/**
+ * Loads data from page and returns an array with all the promos formatted as objects of type Promo.
+ * @param {string} url
+ * @returns
+ */
+const scrapURL = async (url) => {
+  const promos = [],
+    options = { timeout: 3000 },
+    response = await got(url, options),
+    $ = cheerio.load(response.body, { decodeEntities: false });
+
+  /** We filter those thread deals that are not expired */
+  $(".thread--deal:not(.thread--expired)").each((_, article) => {
+    const promo = Promo.newInstance($(article));
+    promos.push(promo);
+  });
+  return promos;
+};
+
+/**
+ * Broadcast message to Telegram Channel
+ * @param {string} message
+ * @returns Promise
+ */
+const sendMessageTelegram = async (message) => {
+  const params = {
+    timeout: 3000,
+    searchParams: {
+      chat_id: TELEGRAM_CHAT_ID,
+      text: message,
+    },
+  };
+  return got(TELEGRAM_URL, params);
+};
+
+/**
+ * Broadcast message to Twitter Timeline
+ * @param {string} message
+ * @returns Promise
+ */
 const sendTweet = async (status, oauth) => {
-    const postBody = {
-        status
-    }
-    await oauthPost(postBody, oauth)
-}
+  const postBody = {
+    status,
+  };
+  await oauthPost(postBody, oauth);
+};
 
+/**
+ * Authorized method to post on Twitter.
+ * @param {Object} body
+ * @param {OAuth} oauth
+ */
 const oauthPost = async (body, oauth) => {
-    oauth.post('https://api.twitter.com/1.1/statuses/update.json',
-        TWITTER_USER_ACCESS_TOKEN, TWITTER_USER_SECRET,
-        body,
-        '',
-        err => {
-            if (err) {
-                console.log(err);
-                return err;
-            } else {
-                return true;
-            }
-        }
-    )
-}
-
-exports.handler = async () => {
-    let body = {};
-    let statusCode = 0;
-
-    try {
-        await compose(
-            iterateSites,
-            mergeRetrievedPromos,
-            removeRepeated,
-            getDBItems,
-            filterPromos,
-            storePromos,
-            broadcast
-        )(sites);
-        body = `Elements saved successfully`
-        statusCode = 200;
-    } catch (err) {
-        console.error(err);
-        body = `There was an error on the request: ${err}`;
-        statusCode = 403;
+  oauth.post(
+    "https://api.twitter.com/1.1/statuses/update.json",
+    TWITTER_USER_ACCESS_TOKEN,
+    TWITTER_USER_SECRET,
+    body,
+    "",
+    (err) => {
+      if (err) {
+        console.log(err);
+        return err;
+      } else {
+        return true;
+      }
     }
+  );
+};
 
-    return {
-        statusCode,
-        body
-    };
-}
+/**
+ * AWS Lambda Handler
+ * @returns Object
+ */
+exports.handler = async () => {
+  let body = {};
+  let statusCode = 0;
 
+  try {
+    await compose(
+      iterateSites,
+      mergeRetrievedPromos,
+      removeRepeated,
+      getDBItems,
+      filterPromos,
+      storePromos,
+      broadcast
+    )(sites);
+    body = `Elements saved successfully`;
+    statusCode = 200;
+  } catch (err) {
+    console.error(err);
+    body = `There was an error on the request: ${err}`;
+    statusCode = 403;
+  }
 
-
-
-
+  return {
+    statusCode,
+    body,
+  };
+};
